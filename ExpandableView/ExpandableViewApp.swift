9a54cc0488cc8d25a1da9b98bc899090f56b53b3
githubuiux <uiuxista@gmail.com>
2023-11-09 22:57:52 +0300

    Initial Commit

---
+++ ExpandableView/ExpandableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableViewApp.swift
+//  ExpandableView
+//
+//  Created by Levent on 9.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
